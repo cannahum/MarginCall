25e0cd0f45f21fb993b0fda6e57f0b5f45d017fe
Jon Nahum <cannahum@outlook.com>
2015-02-28 17:36:28 -0500

    some mailer files have been added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# sending emails
+
+MarginCall::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		address: "smtp.gmail.com",
+		port: 587,
+		domain: "imap.gmail.com",
+		authentication: "margincall.cosi166b",
+		password: "margincall12345",
+		enable_starttls_auto: true
+	}
+end
