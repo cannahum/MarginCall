6ecc0c741252d53d8f08c6ef5da6979e9c2538b5
Jon Nahum <cannahum@outlook.com>
2015-03-10 14:02:38 -0400

    testing github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
           params[:session][:remember_me] == '1' ? remember(user) : forget(user)
           redirect_to user
       else
+        #fuck shit 
         flash.now[:danger] = 'Invalid email/password combination'
         render 'new'
       end  
