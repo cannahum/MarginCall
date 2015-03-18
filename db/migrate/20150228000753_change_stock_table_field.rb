adc628823acf39006ddce89fded93f3fba97556e
YosephKant <joseph.kantorovich@gmail.com>
2015-03-18 15:08:08 -0400

    Deleted stock field alter db table

--- db/migrate/20150228000753_change_stock_table_field.rb
+++
@@ -1,8 +0,0 @@
-# class ChangeStockTableField < ActiveRecord::Migration
-#   def change
-#   	change_table :stocks do |entry|
-#   		# entry.remove :trigger_price
-#   		entry.decimal :current_price
-#   	end
-#   end
-# end
