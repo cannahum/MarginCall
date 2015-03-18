91b35a66a51dc5329d2fad63a55c79eeaf52dbe5
YosephKant <joseph.kantorovich@gmail.com>
2015-03-18 15:04:24 -0400

    commented out trigger_price in db/migrate change stocks

--- db/migrate/20150228000753_change_stock_table_field.rb
+++ db/migrate/20150228000753_change_stock_table_field.rb
@@ -1,8 +1,8 @@
-class ChangeStockTableField < ActiveRecord::Migration
-  def change
-  	change_table :stocks do |entry|
-  		# entry.remove :trigger_price
-  		entry.decimal :current_price
-  	end
-  end
-end
+# class ChangeStockTableField < ActiveRecord::Migration
+#   def change
+#   	change_table :stocks do |entry|
+#   		# entry.remove :trigger_price
+#   		entry.decimal :current_price
+#   	end
+#   end
+# end
