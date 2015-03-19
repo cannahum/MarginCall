8cbdd501359238481e5b31b3c49d726f7594c71d
YosephKant <joseph.kantorovich@gmail.com>
2015-03-18 21:14:18 -0400

    fixed stock table

--- db/migrate/20150228000753_change_stock_table_field.rb
+++ db/migrate/20150228000753_change_stock_table_field.rb
@@ -2,7 +2,6 @@ class ChangeStockTableField < ActiveRecord::Migration
   def change
   	change_table :stocks do |entry|
   		entry.remove :trigger_price
-  		entry.decimal :current_price
   	end
   end
 end
