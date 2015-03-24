bd86f3128a9e85b89f1847ec9ddf3dae52dd1273
YosephKant <joseph.kantorovich@gmail.com>
2015-03-24 19:50:15 -0400

    I'm going insane with heroku

--- app/services/triggerservice.rb
+++ app/services/triggerservice.rb
@@ -1,30 +1,30 @@
-# Probably need the texting, email
+# # Probably need the texting, email
 
-# this class is a service through which the application will be checking if a trigger
-# is realized. If the stock price has reached a user's trigger, then the user gets
-# notified.
-class TrigerService < ActiveRecord::Base
+# # this class is a service through which the application will be checking if a trigger
+# # is realized. If the stock price has reached a user's trigger, then the user gets
+# # notified.
+# class TrigerService < ActiveRecord::Base
 
-	Trigger.all.each do |trigger_record|
-		current_price = Stock.find(trigger_record[1])
-		difference = ((trigger_record[2] - current_price) / trigger_record[2]).abs
-		if difference == 0
-			contacttrigger trigger_record
-		elsif difference <= 0.01
-			contact1 trigger_record current_price
-		elsif difference <= 0.05
-			contact5 trigger_record current_price
-		end
-	end
+# 	Trigger.all.each do |trigger_record|
+# 		current_price = Stock.find(trigger_record[1])
+# 		difference = ((trigger_record[2] - current_price) / trigger_record[2]).abs
+# 		if difference == 0
+# 			contacttrigger trigger_record
+# 		elsif difference <= 0.01
+# 			contact1 trigger_record current_price
+# 		elsif difference <= 0.05
+# 			contact5 trigger_record current_price
+# 		end
+# 	end
 	
 
 
-	def contact5
-	end
+# 	def contact5
+# 	end
 
-	def contact1
-	end
+# 	def contact1
+# 	end
 
-	def contacttrigger
-	end
-end
+# 	def contacttrigger
+# 	end
+# end
