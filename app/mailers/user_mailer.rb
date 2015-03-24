ffc93c19deaae0c9d31f80035fa04ff4a25cdbe8
David Wan <davidxinwan@gmail.com>
2015-03-24 12:50:43 -0400

    trigger email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,16 @@ class UserMailer < ApplicationMailer
 	default from: "margincall.cosi166b@gmail.com"
 	def welcome_email(user)
 		@user = user
-		mail(to: @user.email1, subject: 'test email')
+		mail(to: @user.email1, subject: 'Welcome to Margin Call')
 	end
+    
+    def trigger_email(user, trigger)
+        @user = user
+        @trigger= trigger
+        mail(to: @user.email1, subject: "#{@trigger.ticker} Trigger Alert")
+    end
+    
+    
+    
+    
 end
