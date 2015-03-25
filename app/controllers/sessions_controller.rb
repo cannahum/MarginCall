187aba8749dcc9f49e8ae93681d55ba3d15e944a
Saul Tbeile <saultbeile@outlook.com>
2015-03-25 14:37:12 -0400

    Added friendlyforwarding and basic authorization. WIPO

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         # Log the user in and redirect to user's show page
           log_in user
           params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-          redirect_to user
+          redirect_back_or user
       else
         #fuck shit 
         flash.now[:danger] = 'Invalid email/password combination'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ require 'pry'
 require 'pry-byebug'
 
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update,]
+  before_action :correct_user, only: [:edit,:update]
+
 
   # GET /users
   # GET /users.json
@@ -23,6 +25,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
   end
 
   # POST /users
@@ -46,6 +49,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    @user = User.find(params[:id])
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: "User #{@user.name} was successfully updated." }
@@ -60,6 +64,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     respond_to do |format|
       format.html { redirect_to homepage_url, notice: 'User was successfully destroyed.' }
@@ -69,12 +74,21 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation, :email1, :email2, :p_number)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
