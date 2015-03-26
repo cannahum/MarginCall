167123a10ca0d99ed0ca12e70e280beb94821c93
Saul Tbeile <saultbeile@outlook.com>
2015-03-25 22:20:13 -0400

    bug fixes and created admin column in users table, you will need to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306221645) do
+ActiveRecord::Schema.define(version: 20150325190618) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -37,9 +37,10 @@ ActiveRecord::Schema.define(version: 20150306221645) do
     t.string   "email1"
     t.string   "email2"
     t.string   "p_number"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email1"], name: "index_users_on_email1", unique: true
