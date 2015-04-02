49597a07217d45c462f137fddd002f4211e3122a
Saul Tbeile <saultbeile@outlook.com>
2015-04-01 20:14:00 -0400

    2 integration test are running for users_edit_test, kind of complete if you squint really hard and turn your head

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,18 +4,40 @@ class UsersEditTest <ActionDispatch::IntegrationTest
 
 	def setup
 		@user = users(:michael)
+		@other_user = users(:susannah)
 	end
 
-	test "unsuccessful edit" do
+	test "unsuccessful edit fails" do
+		login_as(@user)
 		get edit_user_path(@user)
 		assert_template 'users/edit'
 		patch user_path(@user), user: {
 			name: '',
-			email: "food@food",
+			email1: "food@food",
 			password: "foo",
 			password_confirmation: "dbar"
 		}
 		assert_template 'users/edit'
 	end
+
+	test "user can successfully edit" do
+		login_as(@user)
+		get edit_user_path(@user)
+		assert_template 'users/edit'
+		name = "Paul"
+		email = "food@food.com"
+		patch user_path(@user), user: {
+			name: name,
+			email1: email,
+			password: "monkey",
+			password_confirmation: "monkey"
+		}
+		assert_not flash.empty?
+		assert_redirected_to @user
+		@user.reload
+		assert_equal @user.name, name
+		assert_equal @user.email1, email
+	end
+
 
 end
