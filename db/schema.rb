e948b5bc847b52783efebbcc6e9b95677a128de9
Saul Tbeile <saultbeile@outlook.com>
2015-04-22 14:54:11 -0400

    form shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150416184755) do
 
   create_table "historical_stock_prices", force: :cascade do |t|
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 20150416184755) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
-  
+
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
     t.string   "company_name"
