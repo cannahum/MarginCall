0f37fedb5a17bc197034ac27a9a128b79aafc2a7
Saul Tbeile <saultbeile@outlook.com>
2015-04-30 15:01:09 -0400

    Added precision to the daily min and max price, please db reset

--- db/migrate/20150306221645_add_daily_min_max_price_to_stocks.rb
+++ db/migrate/20150306221645_add_daily_min_max_price_to_stocks.rb
@@ -1,6 +1,6 @@
 class AddDailyMinMaxPriceToStocks < ActiveRecord::Migration
   def change
-    add_column :stocks, :daily_min_price, :decimal
-    add_column :stocks, :daily_max_price, :decimal
+    add_column :stocks, :daily_min_price, :decimal, precision: 8, scale: 2
+    add_column :stocks, :daily_max_price, :decimal, precision: 8, scale: 2
   end
 end
