ce0ca727f6e3e8ba74e32dc1a32a71efd5d276be
David Wan <davidxinwan@gmail.com>
2015-05-05 14:14:55 -0400

    views

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -49,8 +49,8 @@ class TriggersController < ApplicationController
           @trigger.comparison = params.fetch(:comparison)
           @trigger.save
           TriggerService.unique_stock @trigger.ticker
-          format.html { redirect_to @trigger, notice: 'Trigger was successfully created.' }
-          format.json { render :show, status: :created, location: @trigger }         
+          format.html { redirect_to user_path(current_user), notice: 'Trigger was successfully created.' }
+          #  format.json { render :show, status: :created, location: @trigger }
         else
           format.html { render :new }
           format.json { render json: @trigger.errors, status: :unprocessable_entity }
